737f8f82feefaee24c9b29361c99a4a53ce50451
phil <uniphil@gmail.com>
2021-01-12 22:43:32 -0500

    Add a terrible dependency pinning helper
    should probably just get on with it and install/learn pipenv, but this was kind
    of fun and plus, it runs slow so you know it's doing a lot of important work.

---
+++ freeze.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -eu
+msg() {
+    word="${1-}"; shift
+    case "$word" in
+        Create | Install | Update) COLOR='1;36' ;;  # green
+        Clean | Check)             COLOR='1;33' ;;  # yellow
+        Freeze)                    COLOR='1;36' ;;  # cyan
+        Ohno)                      COLOR='1;31' ;;  # red
+        -?*)                       COLOR='1;37' ;;  # white
+    esac
+    echo -e " \033[1;37m---> \033[1m\033[${COLOR}m${word}\033[1;37m ${*}\033[0m" >&2
+}
+etab() {
+    if [ -t 1 ]
+        then sed 's/^/      /'
+        else cat
+    fi
+}
+did_not_match() {
+    msg Ohno the new freeze differs
+    diff -U1 requirements.freezing.txt requirements.txt | etab
+    [ -t 1 ] || exit 1  # end here if we're not in an interactive shell
+    msg Freeze these changes to requirements.txt?
+    [[ "$(read -ep ' [yN] '; echo $REPLY)" == [Yy]* ]] || exit 1 && {
+        msg Update requirements.txt
+        cp requirements.freezing.txt requirements.txt
+    }
+}
+
+msg Create temporary virtualenv venv.freezing
+python3 -m venv venv.freezing
+
+msg Install dependencies to venv.freezing
+venv.freezing/bin/pip --disable-pip-version-check install -qr requirements.thawed.txt
+
+msg Freeze packages in venv.freezing to requirements.freezing.txt
+venv.freezing/bin/pip freeze > requirements.freezing.txt
+
+msg Clean up venv.freezing
+rm -fr venv.freezing/
+
+msg Check if the new freeze maches the latest
+diff -q requirements.freezing.txt requirements.txt >/dev/null || did_not_match
+
+msg Clean requirements.freezing.txt
+rm requirements.freezing.txt
