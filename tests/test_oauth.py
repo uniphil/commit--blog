8be6aecfdae04a9c382263195ccb2141716e005b
phil <uniphil@gmail.com>
2021-12-10 20:17:14 -0500

    test reusing authorization code
    make sure we can't do it

--- tests/test_oauth.py
+++ tests/test_oauth.py
@@ -4,8 +4,6 @@ from os import urandom
 import urllib.parse
 
 
-# todo test: can an authorization code be used again to make another token? (hopefully no)
-
 def test_oauth_flow_anonymous(oauth_app, client):
     state = '31zy-J0AN1s2fQ73uTHLtQ'
     code_challenge = 'ElnII37xeSswIXzCQ8eINgEe55WxY1dvp0KU9xlBV8I'
@@ -120,3 +118,64 @@ def test_oauth_flow_deny(oauth_app, login, gh_blogger):
         })
         assert allow_resp.status_code == 401, allow_resp.data
         assert b'authorization denied' in allow_resp.data
+
+
+def test_oauth_flow_reuse_authorization(oauth_app, login, gh_blogger):
+    app_name = oauth_app.client_metadata["client_name"]
+    authorized_url = 'http://localhost:33205/oauth/authorized'
+
+    with login(gh_blogger) as client:
+        code_verifier = urandom(48).hex()
+        code_challenge = b64(sha256(code_verifier.encode()).digest()).decode().replace('=', '')
+        state = urandom(16).hex()
+
+        auth_qs = {
+            'response_type': 'code',
+            'client_id': oauth_app.client_id,
+            'state': state,
+            'code_challenge': code_challenge,
+            'code_challenge_method': 'S256',
+            'redirect_uri': authorized_url,
+            'scope': 'blog',
+        }
+        auth_resp = client.get('/oauth/auth', query_string=auth_qs)
+        assert auth_resp.status_code == 200, auth_resp.data
+        assert f'Allow "{app_name}" access?'.encode() in auth_resp.data
+        assert b'Authorize' in auth_resp.data
+
+        allow_resp = client.post('/oauth/auth', query_string=auth_qs, data={
+            'authorize': '',
+            'csrf_token': client.csrf_token,
+        })
+        assert allow_resp.status_code == 302, allow_resp.data
+        redir_url, qs = allow_resp.headers['location'].split('?', 1)
+        assert redir_url == authorized_url
+        query = dict(urllib.parse.parse_qsl(qs))
+        assert query['state'] == state
+        assert 'code' in query
+
+        # get a token with the authorization code
+        token_resp = client.post('/oauth/token', data={
+            'grant_type': 'authorization_code',
+            'client_id': oauth_app.client_id,
+            'code': query['code'],
+            'code_verifier': code_verifier,
+            'redirect_uri': authorized_url,
+        })
+        assert token_resp.status_code == 200, token_resp.data
+        assert b'access_token' in token_resp.data
+        assert token_resp.json['expires_in'] > 60  # arbitrary -- some seconds in the future
+        assert token_resp.json['scope'] == 'blog'
+        assert token_resp.json['token_type'] == 'Bearer'
+        access_token = token_resp.json['access_token']
+
+        # try to reuse the authorization code to get a second token
+        token_resp = client.post('/oauth/token', data={
+            'grant_type': 'authorization_code',
+            'client_id': oauth_app.client_id,
+            'code': query['code'],
+            'code_verifier': code_verifier,
+            'redirect_uri': authorized_url,
+        })
+        assert token_resp.status_code == 400, token_resp.data
+        assert b'Invalid \\"code\\"' in token_resp.data
